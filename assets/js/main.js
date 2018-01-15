c547c7b5357b93ad5dc1dfec42423f1074badecb
Subin George <subinsgm@gmail.com>
2018-01-15 09:52:20 +0530

    First upload
    Included following styles.
    :- Menu
    :- Forms
    :-Buttons
    :- Table

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+//Main Menu
+$('.main-nav ul li:has(ul)').addClass('submenu');
+$('.main-nav ul li:has(ul)').append("<i></i>");
+$('.main-nav ul i').click(function() {
+    $(this).parent('li').toggleClass('open');
+    $(this).parent('li').children('ul').slideToggle();
+})
+
+//Mobile Menu
+$('.mob-btn').click(function() {
+    if (!$('html').hasClass('show-menu')) {
+        $('html').addClass('show-menu');
+    } else {
+        $('html').removeClass('show-menu');
+    }
+});
+
+$('.overlay').click(function() {
+    if ($('html').hasClass('show-menu')) {
+        $('html').removeClass('show-menu');
+    }
+});
